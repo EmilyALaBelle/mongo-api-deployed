bf27438c3fb03a99307a3c4e7ffc0ce599eb53c2
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-20 16:32:28 -0400

    commit

--- functions/src/furniture.js
+++ functions/src/furniture.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb"
 import dbConnect from "./dbConnect.js"
 
 export async function getAllFurniture(req, res) {
@@ -28,4 +29,16 @@ export async function addNewFurniture(req, res) {
         })
     //return responce with 201 all bueno
     res.status(201).send({ message: 'Furniture added' })
+}
+
+export async function updateFurniture(req, res) {
+  const {furnitureId} = req.params
+  const db = dbConnect()
+  await db.collection('furniture')
+  .findOneAndUpdate({_id: new ObjectId(furnitureId)}, {$set: req.body})
+  .catch(err => {
+    res.status(500).send(err)
+    return
+  })
+res.status(202).send({message: 'Updated furniture'})
 }
