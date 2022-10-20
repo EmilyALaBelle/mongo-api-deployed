2b1deb2a292cb1847a5583d6b849bfda5fc4e8f5
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-20 15:26:58 -0400

    commit

--- functions/src/furniture.js
+++ functions/src/furniture.js
@@ -13,3 +13,17 @@ export async function getAllFurniture(req, res) {
     // send back the array of furniture
     res.send(collection)
 }
+
+export async function addNewFurniture(req, res) {
+    //get new furniture from the body of the request
+    const { brand, model, type, price } = req.body
+    const newFurniture = {brand, model, type, price: Number(price)}
+    //connect to database
+    const db = dbConnect()
+    //put this new furniture into our furniture collecion in our db
+    await db.collection('furniture').insertOne(newFurniture)
+    //catch errors and send with status 500
+    
+    //return responce with 201 all bueno
+    res.status(201).send({message: 'Furniture added'})
+}
